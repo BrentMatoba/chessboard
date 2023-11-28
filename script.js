29de1e3baa94323b02e6caad05cedf46d804c4e1
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-11-27 17:55:35 -0800

    add board creation

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 let board = document.getElementById("chessboard")
 
-
+function createBoard(){
+let white = true;
 for(let i=0; i<8; i++){
+    if(isEven(i)){
+        white = true;
+    }
+    else{white=false;}
+
     const row = document.createElement("div")
     for(let j = 0; j<8; j++){
         let square = document.createElement("div")
-        square.classList.add("square")
+
+        if(white==true){
+            square.classList.add("square", "white")
+            white=false;
+        }
+        else{
+            square.classList.add("square", "black")
+            white=true;
+        }
         row.appendChild(square)
     }
     board.append(row)
+}}
+
+function isEven(num){
+    return num % 2 ===0;
 }
+
+createBoard()
